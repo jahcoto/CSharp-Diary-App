c689cdeadb4cf398b76d41b77d7225123e11026b
jahcoto <jahcoto@gmail.com>
2023-01-02 18:26:49 -0600

    Second commit

--- DiaryApp/Form1.Designer.cs
+++ DiaryApp/Form1.Designer.cs
@@ -28,34 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pnlSideBar = new System.Windows.Forms.Panel();
             this.pnlFooter = new System.Windows.Forms.Panel();
             this.tsMenu = new System.Windows.Forms.ToolStrip();
+            this.btnShowDesktop = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
             this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.button1 = new System.Windows.Forms.Button();
+            this.lblDigitalClock = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pnlSideBar.SuspendLayout();
             this.tsMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlSideBar
             // 
             this.pnlSideBar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSideBar.Controls.Add(this.lblDigitalClock);
+            this.pnlSideBar.Controls.Add(this.btnShowDesktop);
             this.pnlSideBar.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlSideBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlSideBar.Margin = new System.Windows.Forms.Padding(2);
             this.pnlSideBar.Name = "pnlSideBar";
-            this.pnlSideBar.Size = new System.Drawing.Size(199, 424);
+            this.pnlSideBar.Size = new System.Drawing.Size(150, 534);
             this.pnlSideBar.TabIndex = 1;
             // 
             // pnlFooter
             // 
             this.pnlFooter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pnlFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlFooter.Location = new System.Drawing.Point(199, 299);
+            this.pnlFooter.Location = new System.Drawing.Point(150, 432);
+            this.pnlFooter.Margin = new System.Windows.Forms.Padding(2);
             this.pnlFooter.Name = "pnlFooter";
-            this.pnlFooter.Size = new System.Drawing.Size(786, 125);
+            this.pnlFooter.Size = new System.Drawing.Size(863, 102);
             this.pnlFooter.TabIndex = 2;
             // 
             // tsMenu
@@ -69,11 +79,49 @@
             this.toolStripButton3,
             this.toolStripButton2,
             this.toolStripButton1});
-            this.tsMenu.Location = new System.Drawing.Point(199, 0);
+            this.tsMenu.Location = new System.Drawing.Point(150, 0);
             this.tsMenu.Name = "tsMenu";
-            this.tsMenu.Size = new System.Drawing.Size(786, 98);
+            this.tsMenu.Size = new System.Drawing.Size(863, 80);
             this.tsMenu.TabIndex = 3;
             this.tsMenu.Text = "toolStrip1";
+            // 
+            // btnShowDesktop
+            // 
+            this.btnShowDesktop.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnShowDesktop.Image = global::DiaryApp.Properties.Resources.computer_icon;
+            this.btnShowDesktop.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnShowDesktop.Location = new System.Drawing.Point(-1, 463);
+            this.btnShowDesktop.Name = "btnShowDesktop";
+            this.btnShowDesktop.Size = new System.Drawing.Size(150, 70);
+            this.btnShowDesktop.TabIndex = 6;
+            this.btnShowDesktop.Text = "Show Desktop";
+            this.btnShowDesktop.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnShowDesktop.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnShowDesktop.UseVisualStyleBackColor = true;
+            this.btnShowDesktop.Click += new System.EventHandler(this.btnShowDesktop_Click);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimize.Image = global::DiaryApp.Properties.Resources.minimize_icon;
+            this.btnMinimize.Location = new System.Drawing.Point(897, 11);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(50, 50);
+            this.btnMinimize.TabIndex = 5;
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Image = global::DiaryApp.Properties.Resources.close_icon;
+            this.btnClose.Location = new System.Drawing.Point(952, 11);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(50, 50);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // toolStripButton5
             // 
@@ -130,33 +178,45 @@
             this.toolStripButton1.Text = "About Us";
             this.toolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             // 
-            // button1
+            // lblDigitalClock
             // 
-            this.button1.Location = new System.Drawing.Point(898, 169);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Close";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.lblDigitalClock.BackColor = System.Drawing.Color.Black;
+            this.lblDigitalClock.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDigitalClock.ForeColor = System.Drawing.Color.White;
+            this.lblDigitalClock.Location = new System.Drawing.Point(-1, 53);
+            this.lblDigitalClock.Name = "lblDigitalClock";
+            this.lblDigitalClock.Size = new System.Drawing.Size(150, 53);
+            this.lblDigitalClock.TabIndex = 6;
+            this.lblDigitalClock.Text = "12:00:00";
+            this.lblDigitalClock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(985, 424);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1013, 534);
+            this.Controls.Add(this.btnMinimize);
+            this.Controls.Add(this.btnClose);
             this.Controls.Add(this.tsMenu);
             this.Controls.Add(this.pnlFooter);
             this.Controls.Add(this.pnlSideBar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "JaHCoto Diary App";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlSideBar.ResumeLayout(false);
             this.tsMenu.ResumeLayout(false);
             this.tsMenu.PerformLayout();
             this.ResumeLayout(false);
@@ -168,12 +228,16 @@
         private System.Windows.Forms.Panel pnlSideBar;
         private System.Windows.Forms.Panel pnlFooter;
         private System.Windows.Forms.ToolStrip tsMenu;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.ToolStripButton toolStripButton5;
         private System.Windows.Forms.ToolStripButton toolStripButton4;
         private System.Windows.Forms.ToolStripButton toolStripButton3;
         private System.Windows.Forms.ToolStripButton toolStripButton2;
         private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnShowDesktop;
+        private System.Windows.Forms.Label lblDigitalClock;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- DiaryApp/Form1.cs
+++ DiaryApp/Form1.cs
@@ -17,9 +17,38 @@ namespace DiaryApp
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            this.Close();
+        private void btnClose_Click(object sender, EventArgs e)
+        {            
+            if (MessageBox.Show("Do you want to exit?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) 
+            {
+                this.Close();
+            }
+            
         }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState= FormWindowState.Minimized;
+        }
+
+        private void btnShowDesktop_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            getTime();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getTime();
+        }
+        public void getTime() 
+        {
+            this.lblDigitalClock.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
     }
 }
